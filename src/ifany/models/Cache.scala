5d0508112f274ca3741ba89805c594f03b6b18cb
Jonas Arnfred <jonas@ifany.org>
2013-03-07 11:01:58 +0000

    Added loadAll method to Cache

--- src/ifany/models/Cache.scala
+++ src/ifany/models/Cache.scala
@@ -7,7 +7,7 @@ object Cache {
 
   val dataField = "data"
   println(Properties.envOrNone("MONGOHQ_URL"));
-  val MongoSetting(mongoDB) = Properties.envOrNone("MONGOHQ_URL")
+  val MongoSetting(mongoDB) = Some(Properties.envOrElse("MONGOHQ_URL", "mongodb://heroku:d9aa08dde373b85276f93b9b44dfdaa8@linus.mongohq.com:10004/app12728917"))
   //val mongoDB = MongoClient(mongoString)(dbName)
 
   def save(id : String, coll : String, data : String) : Unit = {
@@ -26,6 +26,19 @@ object Cache {
     // Then load
     for (col <- mongoColl.findOne(Map("_id" -> id))) yield col(dataField).asInstanceOf[String]
   }
+
+  def loadAll(ids : List[String], coll : String) : Iterator[String] = {
+    // Get the right collection
+    val mongoColl = mongoDB(coll)
+
+    // Prepare id map
+    val idMap = (for (id <- ids) yield ("_id" -> id)).toMap
+
+    // Load all ids
+    for (col <- mongoColl.find("_id" $in ids)) yield col(dataField).asInstanceOf[String]
+  }
+
+
 }
 
 // From:
